d442539eda6fe15c31eef8256ad334880baeb7d7
anastasiia <Anastasiia.Lepova@ivkhk.ee>
2019-12-16 19:48:54 +0200

    Images

---
+++ Lessons/jQuery_lessons/lesson_2/js/main.js
@@ -0,0 +1,7 @@
+//Взаимодействие с элементами//
+
+$('h2, p') //Первый, второй, третий и тд.
+$('h2 span')// outer и inner
+$('h2 > span')// parent >child
+$('h2 + p')// prev + next
+$('h2 ~ p').css('border', '2px solid #000');//prev ~ next
---
+++ Lessons/jQuery_lessons/lesson_6/js/main.js
@@ -0,0 +1,18 @@
+/* ==== Фильтры ==== */ 
+
+/* Фильтры форм в jQuery */
+
+$('input').css('border', '1px solid red'); // Все поля input
+// $('input:button').css('border', '1px solid red'); // Поле button - простая кнопка
+// $('input:radio').css('display', 'block'); // Радио-кнопки
+// $('input:checkbox').css('display', 'block'); // Чукбоксы
+// $('input:text').css('border', '1px solid red'); // Текстовые поля для ввода
+// $('input:password').css('border', '1px solid red'); // Поле для ввода пароля
+// $('input:file').css('border', '1px solid red'); // Поле для загрузки файлов
+// $('input:submit').css('border', '1px solid red'); // Кнопка Отправить
+// $('input:reset').css('border', '1px solid red'); // Кнопка Сбросить
+// $('input:image').css('border', '1px solid red'); // Кнопка-картинка
+// $('input:focus').css('display', 'block'); // Выбранное поле (состояние фокуса)
+// $('input:checked').css('border', '1px solid red'); // Выбранный чекбокс или радио-кнопка
+// $('input:enabled').css('border', '1px solid red'); // Доступное поле
+// $('input:disabled').css('border', '1px solid red'); // Недоступное поле
